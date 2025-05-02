8aa5f28e75ead86da97335da677f667efdeb4e93
HaoVu <caihoncuagiamnguc@gmail.com>
2025-05-03 04:36:03 +0700

    re-run cicd

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
+  // server: {
+  //   port: 3000, // thay 3001 bằng cổng bạn muốn
+  // },
   plugins: [
     tailwindcss(),
   ],
